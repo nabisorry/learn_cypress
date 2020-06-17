42242e29515ddb2bb39108905833b314551be383
nabisorry <cdb131450@gmail.com>
2020-06-17 17:12:07 +0900

    Delete smoke.spec.js

--- cypress/integration/smoke.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Smoke Test', () => {
-  it('can view the home page', () => {
-    // baseUrl + visit 에 방문했을때
-    cy.visit('/');
-    // 'Learn React' 텍스트가 있는지 확인한다.
-    cy.contains('Hello, world.');
-  });
-});
